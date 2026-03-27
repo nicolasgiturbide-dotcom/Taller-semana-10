d96768093556757fa9f2b96a2ae4e37c31a609fc
Martin Iturbide <martiniturbide@gmail.com>
2026-03-27 13:13:52 -0500

    Corregí un problema que no dejaba abrir los arhchivos

---
+++ Ejercicio8-Examen.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int x;
+	int y;
+	cout<<"Ingresa el primer valor: "<<endl;
+	cin>>x;
+	cout<<"Ingresa el segundo valor: "<<endl;
+	cin>>y;
+	cout<<"Suma: "<<x+y<<endl;
+	cout<<"Resta: "<<x-y<<endl;
+	cout<<"Multiplicacion: "<<x*y<<endl;
+	return 0;
+}
+
+//El codigo esta bien, no necesita corregirse :D
+//Adjunto en el repositorio la foto del examen
