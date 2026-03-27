cb7e62b9f6558b568326e8a415732fbb5b969258
Martin Iturbide <martiniturbide@gmail.com>
2026-03-27 13:10:23 -0500

    Agregue la correccion de la prueba

---
+++ Ejercicio9-Examen .cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int x;
+	cout<<"Hasta que numero quieres llegar?"<<endl;
+	cin>>x;
+	for(int i=1;i<=x;i++){
+		cout<<i<<"->"<<i*i<<endl;
+	}
+	return 0;
+}
+
+//El codigo esta bien, no necesita corregirse :D
+//Adjunto en el repositorio la foto del examen
